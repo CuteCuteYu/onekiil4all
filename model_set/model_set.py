c4e8958d5d6c8634b66a63d777a734220f7a3b29
CuteCuteYu <j4543519@hotmail.com>
2026-03-27 04:29:54 +0800

    add

--- model_set/model_set.py
+++ model_set/model_set.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
+import dotenv
+import os
+dotenv.load_dotenv()
 model = ChatOpenAI(
-    api_key="e919bff889bf4861a8e2e70620b868fd.4njSRicEBimaffKQ",
-    base_url=" https://open.bigmodel.cn/api/coding/paas/v4",
     model="glm-4.7",
 )
